0dea534748a5aff7dcfd5fff1c958fc2a5462a3e
lilly <lilly1987@naver.com>
2023-03-18 23:56:42 +0900

    Add files via upload

--- VAELoaderDecode.py
+++ VAELoaderDecode.py
@@ -1,36 +1,33 @@
 import os
 import comfy.sd
-from nodes import recursive_search, filter_files_extensions, supported_pt_extensions
+from nodes import *
+import folder_paths
+
 
 # VAEDecode
 # VAELoader
 # VAELoaderDecode
 
-wd = os.getcwd()
-print("working directory is ", wd)
-
-filePath = __file__
-print("This script file path is ", filePath)
-
-absFilePath = os.path.abspath(__file__)
-print("This script absolute path is ", absFilePath)
-
-realFilePath = os.path.realpath(__file__)
-print("This script real path is ", realFilePath)
-
-path, filename = os.path.split(absFilePath)
-print("Script file path is {}, filename is {}".format(path, filename))
-
+#wd = os.getcwd()
+#print("working directory is ", wd)
+#
+#filePath = __file__
+#print("This script file path is ", filePath)
+#
+#absFilePath = os.path.abspath(__file__)
+#print("This script absolute path is ", absFilePath)
+#
+#realFilePath = os.path.realpath(__file__)
+#print("This script real path is ", realFilePath)
+#
+#path, filename = os.path.split(absFilePath)
+#print("Script file path is {}, filename is {}".format(path, filename))
+        
 class VAELoaderDecode:
 
-    #models_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "models")
-    models_dir = os.path.join(os.getcwd(),"ComfyUI", "models")
-    vae_dir = os.path.join(models_dir, "vae")
-    
     def __init__(self, device="cpu"):
         self.device = device
-        print(f"VAELoaderDecode : {self.models_dir}")
-    
+
     #@classmethod
     #def INPUT_TYPES(s):
     #    return {"required": { "samples": ("LATENT", ), "vae": ("VAE", )}}
@@ -41,10 +38,11 @@ class VAELoaderDecode:
         
     @classmethod
     def INPUT_TYPES(s):
+
         return {
             "required": { 
                 "samples": ("LATENT", ),
-                "vae_name": (filter_files_extensions(recursive_search(s.vae_dir), supported_pt_extensions), )
+                "vae_name": (folder_paths.get_filename_list("vae"), )
             }
         }
         
@@ -64,7 +62,11 @@ class VAELoaderDecode:
     #    return (vae.decode(samples["samples"]), )        
         
     def test(self, vae_name, samples):
-        vae_path = os.path.join(self.vae_dir, vae_name)
+
+        t=folder_paths.get_filename_list("vae")
+        print(f"VAELoaderDecode : {t}")
+        vae_path = folder_paths.get_full_path("vae", vae_name)
+        print(f"VAELoaderDecode : {vae_path}")
         vae = comfy.sd.VAE(ckpt_path=vae_path)
         return (vae.decode(samples["samples"]), )
         
--- random_sampler_node.py
+++ random_sampler_node.py
@@ -4,17 +4,17 @@ import comfy.samplers
 import random
 from nodes import common_ksampler
 
-wd = os.getcwd()
-print("working directory is ", wd)
-
-filePath = __file__
-print("This script file path is ", filePath)
-
-absFilePath = os.path.abspath(__file__)
-print("This script absolute path is ", absFilePath)
-
-path, filename = os.path.split(absFilePath)
-print("Script file path is {}, filename is {}".format(path, filename))
+#wd = os.getcwd()
+#print("working directory is ", wd)
+#
+#filePath = __file__
+#print("This script file path is ", filePath)
+#
+#absFilePath = os.path.abspath(__file__)
+#print("This script absolute path is ", absFilePath)
+#
+#path, filename = os.path.split(absFilePath)
+#print("Script file path is {}, filename is {}".format(path, filename))
 
 
 class Random_Sampler:
